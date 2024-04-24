9eb929d641615e4623a7858cb271b6d52ceed1ab
Kelsey Cordero <kcordero99@gmail.com>
2024-04-24 18:39:48 -0400

    modified changes

--- db/client.js
+++ db/client.js
@@ -1,8 +1,180 @@
 //define connection with database 
 //install pg package
 const { Client } = require('pg');
-//add the server url
-const client = new Client('postgres://localhost:5432/fitness_trackr')
-  //console.log (`client has been defined`, client); //tested
+const client = new Client(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/fitness_trackr')
+
+// USERS
+
+const createUser = async (username, password) => {
+  try{
+    const { rows: [user]} = await client.query(`
+    INSERT INTO users(username, password)
+    VALUES ($1, $2)
+    RETURNING *;
+    `, [username, password]);
+  return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+const getUserById = async (id) => {
+  try{
+    const {rows: [user]} = await client.query(`
+    SELECT * FROM users
+    WHERE id=$1;
+    `, [id]);
+    return user;
+  }catch(error){
+    throw error;
+  }
+}
+
+//ACTIVITIES METHODS START
+
+const createAnActivity = async(activityName, activityDescription) => {
+  try {
+    //deconstruct object and await client query then create row variables
+    const { rows: [activity] } = await client.query(`
+      INSERT INTO activities (name, description)
+      VALUES ($1, $2)
+      RETURNING *;
+    `, [activityName, activityDescription]);
+    return activity;
+  } catch (error) {
+    console.log(`ERROR caught when creatingAnActivity`, error);
+  }
+}
+
+const getAllActivities = async() => {
+  try{
+    const { rows } = await client.query(`
+      SELECT * FROM activities;
+    `);
+    return rows;
+  }catch (error){
+    console.log(error);
+  }
+}
+
+const getActivityById = async (activityId) => {
+  try{
+   const { rows } = await client.query(`
+   SELECT *FROM activities
+   WHERE id = ${activityId}
+   `);
+   return rows;
+  }catch(error){
+    console.log(error);
+  }
+}
+
+const deleteActivityById = async (activityId) => {
+  try{
+    const { rows } = await client.query(`
+    DELETE FROM activities
+    WHERE id ${activityId}
+    RETURNING *;
+    `);
+    if(!rows || rows.length <=0){
+      return "Could not delete table"
+    } else{
+      return "You have successfully deleted the table"
+    }
+  }catch(error){
+    console.log(error);
+  }
+}
+
+//ACTIVITIES METHODS END
+
+
+//ROUTINES METHODS START
+
+const createRoutine = async (routineIs_Public, routineName, routineGoal) => {
+  //try + catch
+  try{
+  //deconstruct object and await client query then create row variables    
+    const { rows: [routine] } = await client.query(`
+      INSERT INTO routines(is_public, name, goal)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `, [routineIs_Public, routineName, routineGoal]);
+    return routine;
+  } catch (error) {
+    console.log(`ERROR caught when creatingRoutine`, error);
+  }
+}
+
+const getAllRoutines = async() => {
+  try{
+    const { rows } = await client.query(`
+      SELECT * FROM routines;
+    `);
+    return rows;
+  }catch(error){
+    throw error;
+  }
+}
+
+const getRoutineById = async (routineId) => {
+  try{
+    const { rows } = await client.query(`
+      SELECT * FROM routines
+      WHERE id = ${routineId};
+    `);
+    return rows;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const deleteRoutineById = async (routineId) => {
+  try{
+    const { rows } = await client.query(`
+      DELETE * FROM routines
+      WHERE id = ${routineId}
+      RETURNING *;
+    `);
+    if(!rows || rows.length <= 0) {
+      return "Could not delete table"
+    } else{
+      return "You have successfully deleted the table"
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+//ROUTINES METHODS END
+
+//COMBO METHOD
+const createRoutines_Activities = async (routines_id, activities_id, count) => {
+  try{
+    //await client query
+    const {rows: [routineAndActivity]} = await client.query(`
+      INSERT INTO routines_activities(routines_id, activities_id, count)
+      VALUES (${routines_id}, ${activities_id}, ${count})
+      RETURNING *;
+    `);
+    return routineAndActivity;
+  } catch (error) {
+    console.log('ERROR caught when creating routines&activities', error);
+  } 
+}
+
 //export the client
-module.exports = client;
+module.exports = {
+  client,
+  createUser,
+  getUserById,
+  createAnActivity,
+  getAllActivities,
+  getActivityById,
+  deleteActivityById,
+  createRoutine,
+  getAllRoutines,
+  getRoutineById,
+  deleteRoutineById,
+  createRoutines_Activities
+};
--- db/seed.js
+++ db/seed.js
@@ -1,76 +1,104 @@
-//1. import the client into this file.
-const client = require('./client.js');
-const { createExercise } = require('./activities.js');
-const { createRoutine } = require('./routines.js');
-const { createRoutines_Activities } = require('./routines_activities.js');
+//imports
+require("dotenv").config();
+const { client, createAnActivity, createRoutine, createUser, createRoutines_Activities } = require('./client');
+const bcrypt = require("bcrypt");
 
-//2. create a drop/delete function 
-const dropTables = async () => {
+//Delete tables
+const dropTables = async() => {
   try{
-    //client query needs to be awaited
-    //5. adding existing tables to drop
+    console.log(`Dropping Tables`);
     await client.query(`
       DROP TABLE IF EXISTS routines_activities;
       DROP TABLE IF EXISTS activities;
       DROP TABLE IF EXISTS routines;
+      DROP TABLE IF EXISTS users;
     `);
   } catch (error) {
     console.log('ERROR caught when deleting tables', error);
   }
 }
 
-//2. create an async function that creates tables
+//Create tables
 const createTables = async() => {
   try { 
-    // 5. create activities table and file
-    // 6. create routines table and file
-    // 7. create activities_routines table and file
+    console.log(`Creating Tables`);
     await client.query(`
-      CREATE TABLE activities (
+      CREATE TABLE users (
         id SERIAL PRIMARY KEY,
-        name VARCHAR(50) UNIQUE NOT NULL,
-        description TEXT
+        username VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(60) NOT NULL
       );
 
+      CREATE TABLE activities (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        description TEXT
+      );
       CREATE TABLE routines (
         id SERIAL PRIMARY KEY,
         is_public BOOLEAN,
-        name VARCHAR(50) UNIQUE NOT NULL,
-        goal INTEGER
+        name VARCHAR(50) NOT NULL,
+        goal TEXT
       );
-
-      CREATE TABLE routines_activities (
+      CREATE TABLE routines_activities(
         id SERIAL PRIMARY KEY,
-        routine_id INT REFERENCES routines(id),
-        activity_id INT REFERENCES activities(id),
+        routines_id INT REFERENCES routines(id),
+        activities_id INT REFERENCES activities(id),
         count INT
-      )
+      );
     `);
   } catch (error) {
     console.log(error);
   }
 }
 
-//3. create a sync function that renders/exports the data into the database
+//Create users
+const createUsers = async() => {
+  try{
+    console.log("creating users");
+    const kelsey = await createUser("Kelsey", await bcrypt.hash("kelspop", 10));
+    const amy = await createUser("Amy", await bcrypt.hash("notlee", 10));
+    const silvy = await createUser("silvester", await bcrypt.hash("eldoo", 10));
+  }catch(error){
+    throw error;
+  }
+}
+
+//rebuild db
 const syncAndSeed = async() => {
-  //4. connect to client file
   await client.connect();
   console.log('SEED.JS HAS CONNECTED TO DATABASE');
   
-  //5. call drop tables
   await dropTables();
-  console.log('TABLES DROPPED');
-
-  //5. call create table
   await createTables();
-  console.log('TABLES CREATED');
-
-  //values will be added to the three functions created (activities, routine, act+rout)
   
-  //4. we need to disconnect from the database
+  //values for the routines table
+  const warmUp = await createRoutine("true", "warm up", "Until warmed up and feeling loose");
+  const armDay = await createRoutine("false", "calisthenic-arms", "Once a week for a year");
+  const legDay = await createRoutine("true", "calisthenic-legs", "Once a week for a year");
+  const backDay = await createRoutine("false", "calisthenic-back", "Once a week for a year");
+  const coreDay = await createRoutine("false", "calisthenic-core", "Once a week for a year");
+  
+  //values for activities table
+  const downwardDog = await createAnActivity("Plank to Downward Dog", "Initiate plank postures, then elevate hips to create an inverted V. Maintain posture and slowly revert to plank stance.")
+  const pushUps = await createAnActivity("Push-Ups", "Initiate plank position with hands placed slightly wider than shoulder-width apart, then lower your body by bending elbows and push back up.")
+  const squats = await createAnActivity("Squats", "Start in standing position with feet should-width apart and imitate the movement of sitting on a chair. Hold that position and then slowly stand back up.")
+  const invertedRows = await createAnActivity("Inverted rows", "Lay on back under a really low monkey bar, grab bar and pull body up to bar.");
+  const deadBug = await createAnActivity("Dead-bug", "Lay on back, lift legs up and bend in 90 degree angle. Hands should be reaching towards the sky. Lower opposite side of hand and leg and hold for 15 seconds, switch.")
+  
+  //values created above calling createUsers
+  await createUsers();
+
+  //values for combo table
+  await createRoutines_Activities(downwardDog.id, warmUp.id, 10);
+  await createRoutines_Activities(pushUps.id, armDay.id, 20);
+  await createRoutines_Activities(squats.id, legDay.id, 50);
+  await createRoutines_Activities(invertedRows.id, backDay.id, 25);
+  await createRoutines_Activities(deadBug.id, coreDay.id, 50);
+
   await client.end();
   console.log('SEED.JS HAS DISCONNECTED FROM DATABASE');
 }
 
-//4. calling function SAS
+//call
 syncAndSeed();
